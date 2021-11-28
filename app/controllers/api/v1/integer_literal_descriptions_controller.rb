73a006d644cbf17851fd41aa3a592fede923629a
yusuke39 <hiranoyusuke39@gmail.com>
2021-11-28 18:27:11 +0900

    sample: API

---
+++ app/controllers/api/v1/integer_literal_descriptions_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::IntegerLiteralDescriptionsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'integer_literal_descriptions/index'
+    end
+  end
+  get 'integer_literal_descriptions/index'
   get 'home/index'
   get 'hello_vue/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
